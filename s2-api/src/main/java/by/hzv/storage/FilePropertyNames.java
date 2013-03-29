74c5614a734510e232fb7bca0a30fa0a1ca93618
Dzmitry Katsiuba <soberaionima@gmail.com>
2013-03-29 08:42:37 +0400

    [/] refactoring

--- s2-api/src/main/java/by/hzv/storage/FilePropertyNames.java
+++
@@ -1,9 +0,0 @@
-package by.hzv.storage;
-
-/**
- * @author <a href="mailto:dkotsubo@wiley.com">Dmitry Kotsubo</a>
- * @since 27.03.2013
- */
-public enum FilePropertyNames {
-    CONTENT_SIZE
-}
--- s2-impl/src/main/java/by/hzv/storage/dao/DataStoreRepository.java
+++
@@ -1,9 +0,0 @@
-package by.hzv.storage.dao;
-
-/**
- * @author <a href="mailto:dkotsubo@wiley.com">Dmitry Kotsubo</a>
- * @since 26.03.2013
- */
-public interface DataStoreRepository {
-
-}
